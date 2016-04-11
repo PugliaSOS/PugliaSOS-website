bffeb1f527143f2fa6b7944a718da874fe84872d
Gianluca Messinese <gianluca.messi1@gmail.com>
2016-04-11 21:58:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 <!DOCTYPE html>
 <html xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
 <head>
-<jdoc:include type="head" />
-<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template.css" type="text/css" />
+  <jdoc:include type="head" />
+  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template.css" type="text/css" />
 </head>
 <body>
-<jdoc:include type="modules" name="top" /> 
-<jdoc:include type="component" />
-<jdoc:include type="modules" name="bottom" />
+  <div class="container">
+    <div class="navbar">
+      <ul>
+        <li><jdoc:include type="modules" name="logo"/></li>
+        <li><button><jdoc:include type="modules" name="button-1"/></button></li>
+      </ul>
+    </div>
+  </div>
 </body>
 </html>
