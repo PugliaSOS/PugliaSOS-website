39cad9d8cba1cb3524b49ee92bfbd4cdf124a916
Kevin Terrone <kevint97@hotmail.it>
2016-04-11 18:44:24 +0200

    Creation of folders and basic files for starting

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php defined('_JEXEC') or die('Restricted access');?>
+<!DOCTYPE html>
+<html xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+<head>
+<jdoc:include type="head" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template.css" type="text/css" />
+</head>
+<body>
+<jdoc:include type="modules" name="top" /> 
+<jdoc:include type="component" />
+<jdoc:include type="modules" name="bottom" />
+</body>
+</html>
