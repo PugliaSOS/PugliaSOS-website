7eee452bb01941577d89cf41147582bf3d0de6cc
Michele Lanotte <miki.mouse97@hotmail.it>
2016-04-11 19:16:30 +0200

    basic files for the template

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
-<?php defined('_JEXEC') or die('Restricted access');?>
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
 <!DOCTYPE html>
-<html xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
-<head>
-<jdoc:include type="head" />
-<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template.css" type="text/css" />
-</head>
-<body>
-<jdoc:include type="modules" name="top" /> 
-<jdoc:include type="component" />
-<jdoc:include type="modules" name="bottom" />
-</body>
+<html xml: lang=" <?php echo $this->language; ?>" lang=" <?php echo $this->language; ?>">
+	
+  <head>
+    <jdoc: include type="head" />
+	<link rel="stylesheet" href=" <?php echo $this->baseurl ?> css/system.css" type="text/css" />
+    <link rel="stylesheet" href=" <?php echo $this->template; ?> /css/template.css" type="text/css" />
+  </head>
+	
+  <body> 
+    <jdoc: include type="modules" name="top" />  
+    <jdoc: include type="component" /> 
+    <jdoc: include type="modules" name="bottom" /> 
+  </body>
 </html>
